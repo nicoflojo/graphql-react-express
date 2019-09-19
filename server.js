b8e03d4aabefd877eb4a82f30ae48bba9e99a505
Nico <nico@skubos.com>
2019-09-19 12:03:44 +0700

    adds schema

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
+
 app.listen(4000, () => {
   console.log('listening..');
 });
